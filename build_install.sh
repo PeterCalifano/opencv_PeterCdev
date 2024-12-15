fa43794f0793610d0cbec028811bcf66482330aa
PeterC <petercalifano.gs@gmail.com>
2024-12-15 18:31:03 +0100

    Updated missing dependency build

--- build_install.sh
+++ build_install.sh
@@ -1,5 +1,11 @@
+# Check if opencv_contrib is in the same root directory as opencv
+if [ ! -d "../opencv_contrib_PeterCdev" ]; then
+    echo "Error: opencv_contrib_PeterCdev not found. Please clone it in the same root directory as opencv."
+    exit 1
+fi
+
 # Generate build files
-cmake -B build -S . -DWITH_VTK=On .. # Use -DWITH_TBB=On if you have TBB
+cmake -B build -S . -DWITH_VTK=ON -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib_PeterCdev/modules # Use -DWITH_TBB=On if you have TBB
 
 # Build the project
 cmake --build build --config Release
